35dc747794d9de28b208a3b141e1da0530ed00a3
Rohit Bhaud <92250506+RohitBhaud@users.noreply.github.com>
2022-05-28 11:19:17 +0530

    second

--- src/App.js
+++ src/App.js
@@ -12,16 +12,22 @@ function App() {
        <Flex minWidth='max-content' alignItems='center' gap='2'>
   <Box p='2'>
     <Heading size='md'><Breadcrumb spacing='8px' >
+    <BreadcrumbItem>
+    <BreadcrumbLink href='#'>Logo</BreadcrumbLink>
+  </BreadcrumbItem>
   <BreadcrumbItem>
-    <BreadcrumbLink href='#'>Home</BreadcrumbLink>
+    <BreadcrumbLink href='#'>Inspiration</BreadcrumbLink>
   </BreadcrumbItem>
 
   <BreadcrumbItem>
-    <BreadcrumbLink href='#'>About</BreadcrumbLink>
+    <BreadcrumbLink href='#'>Find Work</BreadcrumbLink>
   </BreadcrumbItem>
 
   <BreadcrumbItem isCurrentPage>
-    <BreadcrumbLink href='#'>Contact</BreadcrumbLink>
+    <BreadcrumbLink href='#'>Learn Design</BreadcrumbLink>
+  </BreadcrumbItem>
+  <BreadcrumbItem isCurrentPage>
+    <BreadcrumbLink href='#'>Hire Designer</BreadcrumbLink>
   </BreadcrumbItem>
 </Breadcrumb></Heading>
 
@@ -30,7 +36,7 @@ function App() {
   <Spacer />
   <ButtonGroup gap='2'>
     <Button colorScheme='teal'>Sign Up</Button>
-    <Button colorScheme='teal'>Log in</Button>
+    <Button colorScheme='pink'>Log in</Button>
   </ButtonGroup>
 </Flex> 
 
