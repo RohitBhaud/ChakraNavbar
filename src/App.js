48189289d8b0a036340c7615879f15ceabb71f04
Rohit Bhaud <92250506+RohitBhaud@users.noreply.github.com>
2022-05-28 11:10:41 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+
+      import { Flex, Box,Heading,Spacer,ButtonGroup,Button,Breadcrumb,BreadcrumbItem,BreadcrumbLink} from '@chakra-ui/react'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+      <div className='App'>
+    
+
+      
+       <Flex minWidth='max-content' alignItems='center' gap='2'>
+  <Box p='2'>
+    <Heading size='md'><Breadcrumb spacing='8px' >
+  <BreadcrumbItem>
+    <BreadcrumbLink href='#'>Home</BreadcrumbLink>
+  </BreadcrumbItem>
+
+  <BreadcrumbItem>
+    <BreadcrumbLink href='#'>About</BreadcrumbLink>
+  </BreadcrumbItem>
+
+  <BreadcrumbItem isCurrentPage>
+    <BreadcrumbLink href='#'>Contact</BreadcrumbLink>
+  </BreadcrumbItem>
+</Breadcrumb></Heading>
+
+
+  </Box>
+  <Spacer />
+  <ButtonGroup gap='2'>
+    <Button colorScheme='teal'>Sign Up</Button>
+    <Button colorScheme='teal'>Log in</Button>
+  </ButtonGroup>
+</Flex> 
+
+      </div>
+ 
   );
 }
 
